03a40b6430321ed62e4e3540d75998f0f032526b
Lasse Lopperi <lasse.lopperi@iki.fi>
2014-05-10 22:36:57 +0300

    Define minimum point interval between drawn beat lines. Draw bar indices next to the first beats of the visible bars.

--- BeatLinesView.cpp
+++ BeatLinesView.cpp
@@ -4,6 +4,8 @@
 #include <QPen>
 #include <QGraphicsSimpleTextItem>
 #include <QGraphicsLineItem>
+#include <QDebug>
+#include <qglobal.h>
 
 /**
  * @brief Graphics for a single vertical beat line
@@ -21,20 +23,20 @@ private:
 
 BeatLinesView::BeatLineView::BeatLineView(float position, int barIndex, int beatIndex, QGraphicsItem* parent)
 : 	TransformationNode(parent),
-    m_text(new QGraphicsSimpleTextItem(QString("%1.%2").arg(barIndex).arg(beatIndex), this)),
+    m_text(new QGraphicsSimpleTextItem(QString("%1").arg(barIndex), this)),
     m_line(new QGraphicsLineItem(0, 0, 0, 1, this))
 {
     QFont smaller(m_text->font());
     smaller.setPointSize(8);
     m_text->setFont(smaller);
     m_text->setFlags(QGraphicsItem::ItemIgnoresTransformations);
-    m_text->setVisible(false); // TODO: Don't show texts for now
+    m_text->setVisible(beatIndex == 0); // Show only for first beat in bar
     m_text->setPos(0, 1);
 
     QPen pen(Qt::DotLine);
     pen.setCosmetic(true);
     // Emphasize the first beat in bar
-    if (beatIndex == 1) pen.setStyle(Qt::SolidLine);
+    if (beatIndex == 0) pen.setStyle(Qt::SolidLine);
     m_line->setPen(pen);
 
     setPos(position, 0);
@@ -97,24 +99,56 @@ void BeatLinesView::updateBeatLines()
     // Remove old lines
     m_beatLines.clear();
 
-    // Assumer 4/4 time signature
+    if (m_bpm <= 0)
+    {
+        qWarning() << "Invalid bpm for beat line drawing:" << m_bpm;
+        return;
+    }
+
+    // Calculate beat distance in the view with current time scale
+    const double beatStepInSecs = 60.0 / m_bpm;
+    const double beatIntervalInPoints = beatStepInSecs * m_timeScale;
+    const double minPointsBetweenBeats = 30.0;
+
+    // Calculate how many beats can we draw, draw every beatIndexStep'th beat.
+    // Adjust so that first beats are always shown before intra-bar beats (beats 2,3,4).
+    int beatIndexStep = qMax(minPointsBetweenBeats / beatIntervalInPoints, 1.0);
+
+    // Assume 4/4 time signature
     const int beatsPerBar = 4;
-    int barIndex = 1;
-    int beatIndex = 1;
+    switch (beatIndexStep)
+    {
+    case 1:
+    case 2:
+        // 1 and 2 are ok
+        break;
 
-    double beatStep = 60.0 / m_bpm;
+    case 3:
+        // Map 3 to 2
+        beatIndexStep = 2;
+        break;
+
+    default:
+        //For >= 4 make it round down to dividable by 4
+        beatIndexStep = (beatIndexStep / 4) * 4;
+        break;
+    }
+
+    int barIndex = 0;
+    int beatIndex = 0;
+
     QGraphicsItem* thisAsGraphicsItem = this;
-
-    for (float beat = m_timeRange.min + m_beatOffset;
-         beat < m_timeRange.max; beat += beatStep)
+    for (float beat = m_timeRange.min + m_beatOffset; beat < m_timeRange.max; beat += (beatStepInSecs * beatIndexStep))
     {
         m_beatLines.push_back(std::make_shared<BeatLineView>(beat, barIndex, beatIndex, thisAsGraphicsItem));
 
-        if (++beatIndex > beatsPerBar)
+        // Advance beats
+        beatIndex += beatIndexStep;
+        if ((beatIndex % beatsPerBar) == 0)
         {
-            // Current bar ended
-            beatIndex = 1;
-            ++barIndex;
+            // If moving to a new bar advance the bar index
+            barIndex += beatIndex / beatsPerBar;
+            beatIndex = 0;
         }
     }
 }
